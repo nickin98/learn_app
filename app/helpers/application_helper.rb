72ae2af58f6bc93c877d4aae15ed44d372cf13f5
nickin98 <zlo-nic@mail.ru>
2020-09-27 14:50:36 +0500

    4.2.1. Комментарии

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,5 @@
 module ApplicationHelper
+  # Возвращает полный заголовок на основе заголовка страницы.
   def full_title(page_title = '')
     base_title = "Ruby on Rails Tutorial Sample App"
     if page_title.empty?

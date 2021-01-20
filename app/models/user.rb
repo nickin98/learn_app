d8f0c0f1a71a1018a660b4e5a81edfd1c0d80c16
nickin98 <zlo-nic@mail.ru>
2021-01-20 22:10:59 +0500

    6.2. стр.229

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,15 @@
 class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  before_save :email_downcase
   validates :name, presence: true, length: { maximum: 50 }
-  validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }
+  validates :email, presence: true, length: { maximum: 255 }, 
+            format: { with: VALID_EMAIL_REGEX },
+            uniqueness: { case_sensitive: false }
+
+
+  private
+
+  def email_downcase
+    self.email = email.downcase
+  end
 end
